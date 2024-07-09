5c01cf5aa0eac5faf0617442b45e2c8e3f478bf7
joseantonionunes <joseantonionunes2018@gmail.com>
2024-07-09 16:30:12 -0300

    API santander

---
+++ src/main/java/me/dio/model/Card.java
@@ -0,0 +1,47 @@
+package me.dio.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_card")
+public class Card {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String number;
+	
+	@Column(name = "available_limit", precision = 13, scale = 2)
+	private BigDecimal limet;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public BigDecimal getLimet() {
+		return limet;
+	}
+	public void setLimet(BigDecimal limet) {
+		this.limet = limet;
+	}
+	
+	
+}
